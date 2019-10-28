caf3bec5ec202a58dccc34ac05bf370d0872241f
Dogan Ulus <doganulus@gmail.com>
2019-10-27 20:22:07 -0700

    changed csv parser

--- apps/mtl/rymtl.cpp
+++ apps/mtl/rymtl.cpp
@@ -8,12 +8,11 @@
 #include "reelay/targets/stdout/formatter.hpp"
 
 #include "../utils/argparse.hpp"
-#include "../utils/csvstream.hpp"
+#include "../utils/csvparser_modern.hpp"
 
-
-int main(int argc, const char* argv[]) {
+int main(int argc, const char *argv[]) {
   using time_t = double;
-  using input_t = std::map<std::string, std::string>;
+  using input_t = csv::CSVRow;
   using interval_set = reelay::interval_set<time_t>;
 
   argparse::ArgumentParser program("rymtl");
@@ -25,14 +24,14 @@ int main(int argc, const char* argv[]) {
 
   /*  Verbosity
    *  ---------
-   *  2 - Print inputs/output
+   *  2 - Print inputs/output # NotImplementedYet
    *  1 - Print output only
    *  0 - Print violations only
    */
   program.add_argument("--verbose")
       .help("enable output verbosity level (default: 0)")
       .default_value(0)
-      .action([](const std::string& value) { return std::stoi(value); });
+      .action([](const std::string &value) { return std::stoi(value); });
 
   program.add_argument("--discrete")
       .help("enable discrete time semantics")
@@ -42,11 +41,11 @@ int main(int argc, const char* argv[]) {
   program.add_argument("-p", "--period")
       .help("define a unit time period")
       .default_value(0)
-      .action([](const std::string& value) { return std::stoi(value); });
+      .action([](const std::string &value) { return std::stoi(value); });
 
   try {
     program.parse_args(argc, argv);
-  } catch (const std::runtime_error& err) {
+  } catch (const std::runtime_error &err) {
     std::cout << err.what() << std::endl;
     program.print_help();
     exit(0);
@@ -55,54 +54,33 @@ int main(int argc, const char* argv[]) {
   std::string spec = program.get<std::string>("spec");
   std::string filename = program.get<std::string>("filename");
 
-  int verbosity = 0; // program.get<int>("--verbose"); // Temporarily disabled
+  int verbosity = program.get<int>("--verbose");
   time_t period = program.get<int>("--period");
 
-  csvstream csvin(filename);
-  auto header = csvin.getheader();
+  csv::CSVReader reader(filename);
 
-  input_t row;
+  int time;
+  auto header = reader.get_col_names();
 
-  if (program.get<bool>("--discrete")) {
+  if (program.get<bool>("--discrete") or) {
+
     auto network =
         reelay::discrete_timed<time_t>::monitor<input_t>::from_temporal_logic(
             spec);
+
     auto formatter =
         reelay::discrete_timed_setting::make_stdout_formatter<input_t, time_t>(
             network, header, verbosity);
 
     std::cout << formatter->header();
-
-    while (csvin >> row) {
+    for (auto &row : reader) {
       network->update(row);
       std::cout << formatter->output();
     }
 
   } else if (period > 0) {
     auto network =
-      reelay::dense_timed<time_t>::monitor<input_t>::from_temporal_logic(
-            spec);
-    auto formatter = 
-      reelay::dense_timed_setting::make_stdout_formatter<input_t, time_t>(network, header, verbosity);
-
-    std::cout << formatter->header();
-
-    time_t now = 0;
-    while (csvin >> row) {
-      now = now + period;
-      network->update(row, now);
-      std::cout << formatter->output();
-    }
-  } else {
-
-    if (std::find(header.begin(), header.end(), "time") != header.end()) {
-      // Pass
-    } else {
-      throw std::runtime_error("No timestamps in the CSV file");
-    }
-
-    auto network =
-      reelay::dense_timed<time_t>::monitor<input_t>::from_temporal_logic(
+        reelay::dense_timed<time_t>::monitor<input_t>::from_temporal_logic(
             spec);
     auto formatter =
         reelay::dense_timed_setting::make_stdout_formatter<input_t, time_t>(
@@ -110,11 +88,26 @@ int main(int argc, const char* argv[]) {
 
     std::cout << formatter->header();
 
-    while (csvin >> row) {
+    time_t now = 0;
+    for (auto &row : reader) {
+      now = now + period;
+      network->update(row, now);
+      std::cout << formatter->output();
+    }
+
+  } else {
+    auto network =
+        reelay::dense_timed<time_t>::monitor<input_t>::from_temporal_logic(
+            spec);
+    auto formatter =
+        reelay::dense_timed_setting::make_stdout_formatter<input_t, time_t>(
+            network, header, verbosity);
+
+    std::cout << formatter->header();
+
+    for (auto &row : reader) {
       network->update(row);
       std::cout << formatter->output();
     }
   }
-
-  return 0;
 }
